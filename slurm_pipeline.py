383dc97b56f9a4f32f4a6b3b2869598f438e9ba5
Tomasz Kacprzak <tomaszk@phys.ethz.ch>
2026-06-09 18:16:08 +0200

    update

--- slurm_pipeline.py
+++ slurm_pipeline.py
@@ -8,8 +8,6 @@ from simple_slurm import Slurm
 LAUNCH_DIR = Path(os.getcwd())
 LOG_DIR = LAUNCH_DIR / "logs"
 LOG_DIR.mkdir(parents=True, exist_ok=True)
-CONFIG_FILE = LAUNCH_DIR / "config_euclidRR2v2multi.yaml"
-DIR_OUT = LAUNCH_DIR / "euclidRR2v2multi"
 
 def get_logs(name):
 
@@ -52,6 +50,8 @@ def submit(slurm, name, command):
 def submit_paramtables(
     *,
     name,
+    config,
+    dir_out,
     **kwargs,
 ):
 
@@ -70,8 +70,8 @@ def submit_paramtables(
     
     command = f"""
     pixi run uv run python -m cosmogridv11.apps.run_paramtables shell_permutations \\
-        --config="{CONFIG_FILE}" \\
-        --dir_out="{DIR_OUT}" \\
+        --config="{LAUNCH_DIR / config}" \\
+        --dir_out="{LAUNCH_DIR / dir_out}" \\
         --verbosity=info
     """
         
@@ -83,6 +83,8 @@ def submit_paramtables(
 def submit_probemaps(
     *,
     name,
+    config,
+    dir_out,
     **kwargs,
 ):
     slurm_args = {
@@ -99,13 +101,46 @@ def submit_probemaps(
     
     command = f"""
     pixi run uv run python -m cosmogridv11.apps.run_probemaps main \\
-        --config="{CONFIG_FILE}" \\
-        --dir_out="{DIR_OUT}" \\
+        --config="{LAUNCH_DIR / config}" \\
+        --dir_out="{LAUNCH_DIR / dir_out}" \\
         --num_maps_per_index=10 \\
         --indices="$SLURM_ARRAY_TASK_ID" \\
         --verbosity=info
     """
     
+    
+    job_id = submit(slurm, name, command)
+    return job_id
+
+def submit_grid_postprocessing(
+    *,
+    name,
+    config,
+    dir_in,
+    dir_out,
+    **kwargs,
+):
+    slurm_args = {
+        "cpus_per_task": 8,
+        "mem": "15600M",
+        "time": "12:00:00",
+        "partition": "performance",
+        "clusters": "cluster",
+    }
+    slurm_args.update(kwargs)
+    slurm_args.update(get_logs(name))
+    slurm = Slurm(**slurm_args)
+    
+    command = f"""
+    pixi run uv run python -m msfm.apps.run_grid_postprocessing
+        --n_files=2500 
+        --config=config_msfm_default.yaml 
+        --dir_in={LAUNCH_DIR / dir_in} 
+        --dir_out={LAUNCH_DIR / dir_out} 
+        --cosmogrid_version="1.1" 
+        --indices="$SLURM_ARRAY_TASK_ID"
+        --verbosity=info
+    """
     
     job_id = submit(slurm, name, command)
     return job_id
@@ -121,10 +156,23 @@ def submit_probemaps(
 ########################################################################################
 ########################################################################################
 
-
+# Make shell permutation tables
 # pixi run uv run python -m cosmogridv11.apps.run_paramtables shell_permutations --config=config_euclidRR2v2multi.yaml   --dir_out=euclidRR2v2multi/ --verbosity=debug
-# permtables_job_id = submit_paramtables(name="permtables")
+permtables_job_id = submit_paramtables(name="permtables"
+                                       config="config_cosmogridv11_EuclidDR1F.yaml", 
+                                       dir_out="EuclidDR1F_cosmogridv11")
 
-# pixi run uv run python -m cosmogridv11.apps.run_probemaps main --config=config_euclidRR2v2multi.yaml --dir_out=euclidRR2v2multi --num_maps_per_index=10 --indices="17" --verbosity=info
-job_id = submit_probemaps(name="euclid_proj_test", array=[0]+list(range(17, 32)))
+# Make projected probe maps
+# pixi run uv run python -m cosmogridv11.apps.run_probemaps main --config=config_EuclidDR1F.yaml --dir_out=EuclidDR1F --num_maps_per_index=10 --indices="17" --verbosity=info
+job_id = submit_probemaps(name="euclid_proj_test", 
+                          config="config_cosmogridv11_EuclidDR1F.yaml", 
+                          dir_out="EuclidDR1F_cosmogridv11", 
+                          array=[0]+list(range(17, 32)))
 
+# Make tfrecords for probe deep learning training
+# pixi run uv run python -m msfm.apps.run_grid_postprocessing --n_files=2500 --config=config_msfm_EuclidDR1F.yaml --dir_in=EuclidDR1F_cosmogridv11/CosmoGrid/bary/ --dir_out=EuclidDR1F_tfrecords/ --cosmogrid_version="1.1" --indices='0'
+job_id = submit_grid_postprocessing(name="euclid_tfrecords", 
+                                    config="config_msfm_EuclidDR1F.yaml", 
+                                    dir_in="EuclidDR1F_cosmogridv11/CosmoGrid/bary/", 
+                                    dir_out="EuclidDR1F_tfrecords", 
+                                    array=[0])
